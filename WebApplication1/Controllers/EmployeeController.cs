eac3f9629e472a68a62c2708306ab44c26891e1d
Sach Pap <sn18papnoi@gmail.com>
2018-05-29 14:28:00 +0530

    Job is done

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeRepository employeeRepo = new EmployeeRepository();
+        public ActionResult Index()
+        {
+            List<Employee> lstEmployee = employeeRepo.GetEmployee();
+            return View(lstEmployee);
+        }
+        public ActionResult Add()
+        {
+            ViewBag.Country = employeeRepo.GetCountries();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Employee e)
+        {
+            bool isSuccess = employeeRepo.InserEmployee(e);
+            return RedirectToAction("Index", "Employee");
+        }
+    }
+}
---
+++ WebApplication1/Models/EmployeeRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class EmployeeRepository
+    {
+        DBDataContext db = new DBDataContext();
+        public List<Employee> GetEmployee()
+        {
+            return db.Employees.ToList();
+        }
+
+        public List<Country> GetCountries()
+        {
+            return db.Countries.ToList();
+        }
+
+        public bool InserEmployee(Employee e)
+        {
+            bool isSuccess = false;
+            try
+            {
+                db.Employees.InsertOnSubmit(e);
+                db.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                isSuccess = false;
+            }
+            return isSuccess;
+        }
+    }
+}
